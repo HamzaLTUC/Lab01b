80492e0edd925233e96c809fdbce5e002b5ec75c
Hamza Qahoush <haamza.qah@gmail.com>
2021-02-28 16:14:57 +0200

    This is my worl for lab02

---
+++ Lab01b.js
@@ -0,0 +1,6 @@
+let FirstName=prompt('What is Your First name ?') ;
+let LastName=prompt('What is Your Last name ?') ;
+let hoppy=prompt('What is Your Hoppy ?') ;
+let drink=prompt('What is Your favourite drink ?') ;
+alert('Hello Guest :) Your name is ' +FirstName  + '  '+ LastName +' ' + 'and Your hoppy is ' +hoppy + '& Your Favourite drink is '+drink +'!');
+console.log(FirstName); console.log(LastName); console.log(hoppy) ; console.log(drink); 
